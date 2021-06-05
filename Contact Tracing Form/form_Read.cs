7168ecae9e851c8733b2b792efe361c51b36ad65
niyuhnuh <nianiaandrea@gmail.com>
2021-06-05 11:41:01 +0800

    Read Form

---
+++ Contact Tracing Form/form_Read.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Contact_Tracing_Form
+{
+    public partial class form_Read : Form
+    {
+        public form_Read()
+        {
+            InitializeComponent();
+        }
+    }
+}
