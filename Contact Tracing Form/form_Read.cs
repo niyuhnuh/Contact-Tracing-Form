2d6180cc9590cf8f63ee0810db9d1e8a7b2f87d6
niyuhnuh <nianiaandrea@gmail.com>
2021-06-05 15:01:40 +0800

    Reset code for datatable

--- Contact Tracing Form/form_Read.cs
+++ Contact Tracing Form/form_Read.cs
@@ -11,7 +11,6 @@ namespace Contact_Tracing_Form
 {
     public partial class form_Read : Form
     {
-        public DataTable tb_SavedInfo = new DataTable("Saved Information");
        
         public form_Read()
         {
@@ -20,23 +19,7 @@ namespace Contact_Tracing_Form
 
         private void form_Read_Load(object sender, EventArgs e)
         {
-            DataTable saved = new DataTable();
-            string[] column_Names = new string[] { "First Name", "Last Name", "Address", "Birthdate", "Gender", "Contact Information", "Time Logged"};
-
-            List<string> info = new List<string>();
-            using (StreamReader readInfo = File.OpenText(@"C:\Users\Annie-Max\source\repos\Contact Tracing Form\Contact Tracing Form\bin\Debug\netcoreapp3.1"))
-            {
-                while (!readInfo.EndOfStream)
-                {
-                    info.Add(readInfo.ReadLine());
             
-                }
-            }
-
-            for (int x = 0; x < 8; x++)
-            {
-                saved.Columns.Add();
-            }
         }
     }
 }
