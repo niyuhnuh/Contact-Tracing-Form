6d6a27b1f762cf44190ed39502ecb28685fef49f
niyuhnuh <nianiaandrea@gmail.com>
2021-06-05 13:01:15 +0800

    Adding separators for datatable (1)

--- Contact Tracing Form/Form1.cs
+++ Contact Tracing Form/Form1.cs
@@ -241,8 +241,8 @@ namespace Contact_Tracing_Form
                 savedInfo.WriteLine(txtbx_Address.Text + ", ");
                 savedInfo.WriteLine(cmb_Month.SelectedItem + ", " + cmb_Day.SelectedItem + ", " + cmb_Year.SelectedItem + ", ");
                 savedInfo.WriteLine(txtbx_Gender.Text + ", ");
-                savedInfo.WriteLine("Contact Information: " + txtbx_Contact1.Text + " | " + txtbx_Contact2.Text);
-                savedInfo.WriteLine("Time Logged: " + txtbx_Time.Text + " " + cmb_AMPM.SelectedItem);
+                savedInfo.WriteLine(txtbx_Contact1.Text + ", " + txtbx_Contact2.Text + ", ");
+                savedInfo.WriteLine(txtbx_Time.Text + cmb_AMPM.SelectedItem + ", ");
                 savedInfo.Close();
 
                 txtbx_FName.Text = "";
