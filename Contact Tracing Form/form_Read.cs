5edfdc4cd3b383fbb4d1e663f919fea8129f4c8a
niyuhnuh <nianiaandrea@gmail.com>
2021-06-05 13:32:56 +0800

    Using while instead of ReadAllLines due to lacking a system assembly

--- Contact Tracing Form/form_Read.cs
+++ Contact Tracing Form/form_Read.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Contact_Tracing_Form
@@ -19,6 +20,15 @@ namespace Contact_Tracing_Form
 
         private void form_Read_Load(object sender, EventArgs e)
         {
+
+            List<string> info = new List<string>();
+            using (StreamReader readInfo = File.OpenText("saved.txt"))
+            {
+                while(!readInfo.EndOfStream)
+                {
+                    info.Add(readInfo.ReadLine());
+                }
+            }
 
             dg_SavedInfo.DataSource = tb_SavedInfo;
 
