c90ea02b29a463d10c9779eae2745ebc0f0bd3c0
niyuhnuh <nianiaandrea@gmail.com>
2021-06-06 19:05:53 +0800

    Trying to Solve an Exception (1)

--- Contact Tracing Form/form_Read.cs
+++ Contact Tracing Form/form_Read.cs
@@ -33,20 +33,19 @@ namespace Contact_Tracing_Form
             //string[] info_Saved = info.ToArray();
             string[] info_Saved = { "First Name", "Last Name", "Address", "Birthdate",
                                     "Gender", "Contact Info1", "Contact Info2", "Time Logged", "ABC"};
-            string[] data;
+ 
             foreach (string column_Name in info_Saved)
             {
-                data = info_Saved.ToString().Split(',');
-                StreamWriter data_File;
-                data_File = File.CreateText("data.txt");
-                data_File.WriteLine(data);
-                data_File.Close();
+                StreamWriter columns;
+                columns = File.CreateText("col.txt");
+                columns.WriteLine(column_Name);
+                columns.Close();
 
-                string[] info_Rows = new string[data.Length];
-                for (int y = 0; y < data.Length; y++)
+                //string[] info_Rows = new string[data.Length];
+               // for (int y = 0; y < data.Length; y++)
                 {
-                    info_Rows[y] = data[y].Trim();
-                    savedInfo.Rows.Add(info_Rows);
+                   // info_Rows[y] = data[y].Trim();
+                   // savedInfo.Rows.Add(info_Rows);
                 }
                 
             }
