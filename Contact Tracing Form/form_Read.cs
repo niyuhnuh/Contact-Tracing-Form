e0da32c573d8aa0db95fa732b99b3b9ff335efe7
niyuhnuh <nianiaandrea@gmail.com>
2021-06-07 09:01:07 +0800

    Reset code for datatable (2)

--- Contact Tracing Form/form_Read.cs
+++ Contact Tracing Form/form_Read.cs
@@ -20,49 +20,7 @@ namespace Contact_Tracing_Form
         private void form_Read_Load(object sender, EventArgs e)
         {
 
-            List<string> info = new List<string>();
-            using (StreamReader readInfo = File.OpenText(@"saved.txt"))
-            {
-                while (!readInfo.EndOfStream)
-                {
-                    info.Add(readInfo.ReadLine());
-                    //For checking only
-                    StreamWriter info_R;
-                    info_R = File.CreateText("info_R.txt");
-                    info_R.WriteLine(info);
-                    //
-                }
-            }
-
-            
-
-           // DataTable savedInfo = new DataTable("Saved Information");
-            //string[] info_Saved = info.ToArray();
-           // string[] info_Saved = { "First Name", "Last Name", "Address", "Birthdate",
-                                    //"Gender", "Contact Info1", "Contact Info2", "Time Logged", "ABC"};
-
-
-
-
-
-            //foreach (string column_Name in info_Saved)
-            {
-               
-
-                //StreamWriter columns;
-                //columns = File.CreateText("col.txt");
-                //columns.WriteLine(column_Name);
-                //columns.Close();
-
-                //string[] info_Rows = new string[data.Length];
-               // for (int y = 0; y < data.Length; y++)
-                {
-                   // info_Rows[y] = data[y].Trim();
-                   // savedInfo.Rows.Add(info_Rows);
-                }
-                
-            }
-
+           
         }
     }
 }
